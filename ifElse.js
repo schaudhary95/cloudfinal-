1bffff09084713dc4442cdf6ccda64cd485c1c45
Simon Chaudhary <schaudhary@mywaketech.edu>
2021-12-01 13:59:40 -0500

    adding files

---
+++ ifElse.js
@@ -0,0 +1,10 @@
+var age = parseFloat(prompt("Enter your age: "));
+if (age < 21)
+{
+    window.location = ("http://www.disney.com/");
+   
+}
+else
+{
+    window.location = ("http://www.espn.com/");
+}
---
+++ whileLoop.js
@@ -0,0 +1,7 @@
+var count = 2
+
+while (count < 101)
+{
+    document.write(count + "<br>");
+    count = count + 2
+}
